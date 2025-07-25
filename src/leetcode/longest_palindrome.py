7060437898dc1f473dc4057357bd5326349e1e51
Daniel Ibarrola <daniel.ibarrola.sanchez@gmail.com>
2025-07-25 10:12:06 -0600

    Longest palindrome

--- src/leetcode/longest_palindrome.py
+++ src/leetcode/longest_palindrome.py
@@ -1,27 +1,37 @@
 class Solution:
     @staticmethod
     def longest_palindrome(words: list[str]) -> int:
-        words_set: set[str] = set(words)
+        word_count: dict[str, int] = {}
+        for word in words:
+            word_count[word] = word_count.get(word, 0) + 1
 
         current_length: int = 0
-        found_self_palindrome: bool = False
+        already_palindrome: bool = False
 
         for word in words:
             reversed_word = word[::-1]
+            count = word_count[word]
 
-            if not found_self_palindrome and word == reversed_word:
-                current_length += 2
-                # words_set.remove(word)
-                found_self_palindrome = True
-                continue
-            elif word == reversed_word:
-                # words_set.remove(word)
-                continue
+            if word == reversed_word:
 
-            # We have found a pair that can form part of a palindrome (e.g) "ab", "ba"
-            if reversed_word in words_set:
-                current_length += 4
-                words_set.remove(reversed_word)
-                words_set.remove(word)
+                if count == 0:
+                    continue
 
-        return current_length
+                if count == 1 and not already_palindrome:
+                    current_length += 1
+                    already_palindrome = True
+                    word_count[word] = 0
+                else:
+                    occurrences_to_use = count - count % 2
+                    current_length += occurrences_to_use
+                    word_count[word] = count - occurrences_to_use
+
+            else:
+                # We have found a pair that can form part of a palindrome (e.g) "ab", "ba"
+                reversed_count = word_count.get(reversed_word, 0)
+                if reversed_count > 0 and count > 0:
+                    current_length += 2
+                    word_count[reversed_word] -= 1
+                    word_count[word] -= 1
+
+        return current_length * 2
--- tests/test_longest_palindrome.py
+++ tests/test_longest_palindrome.py
@@ -13,6 +13,7 @@ class TestLongestPalindrome:
             (["ab", "ty", "yt", "lc", "cl", "ab"], 8),
             (["cc", "ll", "xx"], 2),
             (["ab"], 0),
+            (["em", "pe", "mp", "ee", "pp", "me", "ep", "em", "em", "me"], 14),
         ],
     )
     def test_finds_longest_palindrome_length(self, words: list[str], expected: int):
@@ -22,7 +23,30 @@ class TestLongestPalindrome:
         assert self.solution.longest_palindrome([]) == 0
 
     @pytest.mark.parametrize(
-        "words,expected", [(["aa", "aa"], 2), (["ab", "ab", "ba", "ba"], 8)]
+        "words,expected",
+        [
+            (["aa", "aa"], 4),
+            (["ab", "ab", "ba", "ba"], 8),
+            (
+                [
+                    "dd",
+                    "aa",
+                    "bb",
+                    "dd",
+                    "aa",
+                    "dd",
+                    "bb",
+                    "dd",
+                    "aa",
+                    "cc",
+                    "bb",
+                    "cc",
+                    "dd",
+                    "cc",
+                ],
+                22,
+            ),
+        ],
     )
     def test_find_longest_palindrome_when_words_are_not_unique(
         self, words: list[str], expected: int

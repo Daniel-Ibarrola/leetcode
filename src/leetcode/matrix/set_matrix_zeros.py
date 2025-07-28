f0082c8e7f0ee1d921ed561188be7aeb8a3912cc
Daniel Ibarrola <daniel.ibarrola.sanchez@gmail.com>
2025-07-28 16:02:56 -0600

    Set matrix zeros

--- src/leetcode/matrix/set_matrix_zeros.py
+++ src/leetcode/matrix/set_matrix_zeros.py
@@ -1,6 +1,21 @@
 class Solution:
-    def set_zeroes(self, matrix: list[list[int]]) -> None:
+    @staticmethod
+    def set_zeroes(matrix: list[list[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        pass
+        rows_with_zeros = set()
+        cols_with_zeros = set()
+
+        for ii in range(len(matrix)):
+            for jj in range(len(matrix[ii])):
+                if matrix[ii][jj] == 0:
+                    rows_with_zeros.add(ii)
+                    cols_with_zeros.add(jj)
+
+        for row_index in rows_with_zeros:
+            matrix[row_index] = [0] * len(matrix[row_index])
+
+        for col_index in cols_with_zeros:
+            for row in matrix:
+                row[col_index] = 0
